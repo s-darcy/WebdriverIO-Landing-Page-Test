cc4e0766132198c5cdf0291ab85fa839ec8fd3b3
Shane <sdarcy@redventures.com>
2017-11-21 14:13:00 -0500

    they are still failing

--- test/specs/spec.js
+++ test/specs/spec.js
@@ -2,26 +2,43 @@ var assert = require('assert');
 
 var validURL = 'https://internet.frontier.com/plans-pricing.html';
 
-describe('frontier page', function() {
+describe('frontier page', function () {
 
     //grabs the title
-    it('should get the title of the document', async function () {
+    it('should get the title of the document', function () {
         browser.url('/'); 
         var title = browser.getTitle()
-        await console.log(title);
+        console.log(title);
     });
 
     //tests the Zip finder
-    it('should submit login form', async function () {
+    it('should submit login form', function () {
         browser.url('/');
         browser.setValue('#zip', '22222');
-        await browser.click('#js-track-form-check-availability');
-        var newURL = browser.getUrl();
-        assert.deepEqual(newURL, validURL);
+        browser.click('.js-address-submit');
+        browser.pause(9000);
+        const newURL = browser.getUrl();
+        assert.equal(newURL, validURL);
+    });
+});
+
+describe('checks links', function () {    
+    //making sure links direct correctly
+    it('should link to cart page', function() {
+        browser.url('/');
+        browser.click('.js-track-nav-shop-online');
+        browser.pause(3000);
+        var cart = browser.getUrl();
+        assert.equal(cart, 'https://internet.frontier.com/cart/address');
     });
 
-    // it('should hover', function () {
-    //     var url = browser.url('https://internet.frontier.com/');
+    it('should link to services page', function() {
+        browser.url('/');
+        browser.pause(5000);
+        browser.click('#js-track-home-hero');
+        browser.pause(3000);
+        var cart = browser.getUrl();
+        assert.equal(cart, 'https://internet.frontier.com/services/');
+    });
 
-    // });
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -130,6 +130,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd'
+        // timeout: 99999999
     },
     //
     // =====
